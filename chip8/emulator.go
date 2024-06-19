eb53dc2dd8079f3e9ae41e3dd911da785ec2b752
armadi1809 <azizrmadi@gmail.com>
2024-06-18 21:45:39 -0500

    implemented a couple of opcodes

--- chip8/emulator.go
+++ chip8/emulator.go
@@ -1,6 +1,7 @@
 package chip8
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -17,6 +18,25 @@ type Chip8 struct {
 	stack       [16]uint16
 	sp          uint16
 	key         byte
+}
+
+var fontset []uint8 = []uint8{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 }
 
 func New() *Chip8 {
@@ -36,11 +56,13 @@ func (chip *Chip8) Initialize() {
 
 	// Load fontset
 	for i := range 80 {
-		chip.memory[i] = 0 // TODO: Get the chip 8 fontset
+		chip.memory[i] = fontset[i] // TODO: Get the chip 8 fontset
 
 	}
 
 	// Reset timers
+	chip.delay_timer = 0
+	chip.sound_timer = 0
 }
 
 func (chip *Chip8) LoadProgram(path string) {
@@ -55,9 +77,28 @@ func (chip *Chip8) LoadProgram(path string) {
 }
 
 func (chip *Chip8) EmulateCycle() {
-	chip.opcode = uint16(chip.memory[chip.pc]<<8) | uint16(chip.memory[chip.pc+1])
+	chip.opcode = uint16(chip.memory[chip.pc])<<8 | uint16(chip.memory[chip.pc+1])
 	switch chip.opcode & 0xF000 {
 	// perform opcode translation here
+	case 0xA000:
+		chip.I = chip.opcode & 0x0FFF
+		chip.pc += 2
+	case 0x2000:
+		chip.stack[chip.sp] = chip.pc
+		chip.sp++
+		chip.pc = chip.opcode & 0x0FFF
+	default:
+		fmt.Printf("unkown opcode 0x%X\n", chip.opcode)
+	}
+
+	if chip.delay_timer > 0 {
+		chip.delay_timer--
+	}
+	if chip.sound_timer > 0 {
+		if chip.sound_timer == 1 {
+			fmt.Printf("SIMULATING SOUND: BEEP\n")
+		}
+		chip.sound_timer--
 	}
 
 }
